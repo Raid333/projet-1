99a32c4d0d7a2e440681beecc6991b959154a588
Alexandre <ale.normand@icloud.com>
2017-10-24 11:03:37 +0200

    premier commit

---
+++ NORMAND_ALEXANDRE.php
@@ -0,0 +1,28 @@
+<?php
+
+$n = 47;
+$t = 5;
+
+Function creation_tableau($n,$t) {
+	for ($i=0,$i<$t,$i++) {
+		tab[$i] = $n+3*$i
+	}
+	return tab;
+}
+
+Function produit(tab) {
+	$longueur = leng(tab);
+	for ($y=0,$y<$longueur, $y++) {
+		$i 
+		$produit = $tab[$y] * $tab[$y+1]
+}
+	return $produit;
+}
+
+if ($produit%2 == 0) {
+	echo "le produit est pair";
+} else {
+	echo "le produit est impaire";
+}
+
+?>
